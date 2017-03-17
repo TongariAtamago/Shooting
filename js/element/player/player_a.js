97be32ef6c68db9832a2e52fbf9128a8127fe360
shioleap <shiotsuka.iroha@gmail.com>
2017-03-18 00:30:56 +0900

    ..

--- js/element/player/player_a.js
+++ js/element/player/player_a.js
@@ -5,7 +5,8 @@ PLAYER_TYPE_A.rotationSpeed = 2;//回転スピード
 PLAYER_TYPE_A.rotation = 0;//回転
 PLAYER_TYPE_A.bulletAngle = 90;//弾の打てる角度
 //とりあえず見た目の図形  将来イメージにする
-var PLAYER_TYPE_A_IMAGE = Sprite('player_image_bullet',64,64).addChildTo(PLAYER_TYPE_A);
+var LAYER_TYPE_A_IMAGE = Sprite('player_image_bullet',64,64).addChildTo(PLAYER_TYPE_A);
+var tomapiko = Sprite('tomapiko',64,64).addChildTo(PLAYER_TYPE_A);
 var PLAYER_TYPE_A_COLLISION = CircleShape({
   radius: 4,
   fill: 'blue',
