140334d3252cc2d452a0206cf1fb3a7b1ec37c6a
shioleap <shiotsuka.iroha@gmail.com>
2017-04-03 12:29:44 +0900

    ...

--- js/element/player/player_laser.js
+++ js/element/player/player_laser.js
@@ -1,7 +1,14 @@
-//sh.playerBulletを定義
-phina.define('sh.playerBullet', {
+//sh.playerLaserを定義
+phina.define('sh.playerLaser', {
   //sh.entityクラスを継承
   superClass: 'sh.entity',
+  //敵に当たってる座標
+  hitX : null,
+  hitY : null,
+  //ボディ
+  head : null,
+  body : null,
+  aura : null,
   //初期化
   init: function(type) {
     this.superInit(20);//当たり判定
